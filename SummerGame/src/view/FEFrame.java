0eca8f837a731fedea813832cdeb49818e85d002
Jorge <alexcalibur.3000@gmail.com>
2012-05-20 03:19:17 -0700

    Finished the rough draft of the text boxes

--- SummerGame/src/view/FEFrame.java
+++ SummerGame/src/view/FEFrame.java
@@ -3,6 +3,10 @@ package view;
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
 import javax.swing.JFrame;
 import javax.swing.JLayeredPane;
@@ -14,58 +18,87 @@ public class FEFrame extends JFrame {
 	 * Useless
 	 */
 	private static final long serialVersionUID = 1L;
-	
+
 	// instance variables
 	private static final JLayeredPane lPane = new JLayeredPane();
-	private static final JPanel mapPanel = new JPanel(); // TODO: This should be a custom JPanel later
-	private static final JPanel battlePanel = new JPanel(); // TODO: This should be a custom JPanel later
-	private static final JPanel textPanel = new TextboxPanel(); // TODO: This should be a custom JPanel later
-	
+	private static final JPanel mapPanel = new JPanel(); // TODO: This should be
+															// a custom JPanel
+															// later
+	private static final JPanel battlePanel = new JPanel(); // TODO: This should
+															// be a custom
+															// JPanel later
+	private static final TextboxPanel textPanel = new TextboxPanel(894, 122);
+
 	/**
 	 * Constructs a new Fire Emblem Frame.
 	 * 
-	 * A fire emblem frame is a jframe with support for things such as message dialogs, battle, and etc
+	 * A fire emblem frame is a jframe with support for things such as message
+	 * dialogs, battle, and etc
 	 */
 	public FEFrame() {
-		this.setPreferredSize(new Dimension(900, 700)); // TODO: Decide on a size
-		this.setLayout(new BorderLayout());
+		this.setPreferredSize(new Dimension(900, 700)); // TODO: Decide on a
+														// size
 		this.setResizable(false);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-		
-		mapPanel.setBounds(0, 0, 900, 700);
-		battlePanel.setBounds(100, 150, 700, 400);
+
+		// The dimensions of the content pane on windows is 894x672 with the
+		// 900x700 window size
+		mapPanel.setBounds(0, 0, 894, 672);
+		battlePanel.setBounds(97, 136, 700, 400);
 		textPanel.setBounds(0, 550, 900, 150);
-		
+
 		mapPanel.setBackground(Color.BLUE);
 		battlePanel.setBackground(Color.RED);
-		textPanel.setBackground(Color.GREEN);
 		textPanel.setVisible(false);
 		
-		lPane.add(mapPanel, 0, 0);
-		lPane.add(battlePanel, 1, 0);
-		lPane.add(textPanel, 2, 2);
-		
-		this.add(lPane, BorderLayout.CENTER);
+		// this code make the textbox advance on any keypress
+		textPanel.addKeyListener(new TextboxListener());
+
+		lPane.add(mapPanel, 0, new Integer(0));
+		lPane.add(battlePanel, new Integer(1));
+		lPane.add(textPanel, new Integer(2));
+
+		this.add(lPane);
+		this.setLayeredPane(lPane);
 		this.pack();
 		this.setVisible(true);
 	}
-	
+
 	/**
-	 * This method is not thread safe and should be wrapped inside an invoke later thingy if called from another thread
+	 * This method is not thread safe and should be wrapped inside an invoke
+	 * later thingy if called from another thread
 	 */
-	public static void showTextbox() {
+	public static void showTextbox(String message) {
+		textPanel
+				.setText(message);
 		textPanel.setVisible(true);
-		
-//		try {
-//			Thread.sleep(1500);
-//		} catch (InterruptedException e) {
-//			e.printStackTrace();
-//		}
-		
-//		hideTextbox();
+		textPanel.requestFocusInWindow();
 	}
-	
+
 	private static void hideTextbox() {
 		textPanel.setVisible(false);
+	}
+
+	private static class TextboxListener implements KeyListener {
+
+		@Override
+		public void keyPressed(KeyEvent arg0) {
+			// TODO Auto-generated method stub
+
+	//		System.out.println("HELLO");
+		}
+
+		@Override
+		public void keyReleased(KeyEvent arg0) {
+			if (!textPanel.advanceText())
+				hideTextbox();
+		}
+
+		@Override
+		public void keyTyped(KeyEvent arg0) {
+			// TODO Auto-generated method stub
+
+	//		System.out.println("HELLO");
+		}
 	}
 }
